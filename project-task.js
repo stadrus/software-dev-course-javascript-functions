f322297e2746a8b7ce45d84b82aa100507702f4a
stacey.tadrus@gmail.com <stacey.tadrus@gmail.com>
2025-04-10 14:50:15 -0500

    review

--- project-task.js
+++ project-task.js
@@ -98,7 +98,7 @@ function multiply(num1,num2){
 }
 
 function printMultiply(num1,num2){
-   console.log(` the product of ${num1} and ${num2} is ${multiply(num1,num2)}.`)
+   console.log(`The product of ${num1} and ${num2} is ${multiply(num1,num2)}.`)
 }
 printMultiply(num1,num2)
 
